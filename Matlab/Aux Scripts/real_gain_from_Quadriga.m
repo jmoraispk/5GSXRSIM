774465ce1cc238896efc1d65ce4371e863d8e65e
jmoraispk <joao.morais.jumper@hotmail.com>
2021-05-19 11:29:29 +0100

    add beamforming capability to gain and 3D array plot + fix gain of max

--- Matlab/Aux Scripts/real_gain_from_Quadriga.m
+++ Matlab/Aux Scripts/real_gain_from_Quadriga.m
@@ -13,20 +13,28 @@
 
 % Note: the 3GPP element is dual-polarised. 
 
-N_v = 8;
-N_h = 8;
+N_v = 4;
+N_h = 4;
 N = N_v * N_h;
 
 a = qd_arrayant('3gpp-3d', N_v, N_h, 3.5e9, 1);
 a.coupling = ones(N, 1); % or other precoder (like MRT)
+%a.coupling = p;
 a.combine_pattern();
 a.visualize();
 
+
 % TODO: test the subarray function! a.sub_array(1:64);
 
+[C,I] = max(abs(a.Fa));
+[~,el_idx_of_max] = max(C);
+azi_idx_of_max = I(azi_idx_of_max);
+
+lin_gain = abs(a.Fa(azi_idx_of_max,el_idx_of_max));
+
 disp(['Gains for a ', num2str(N_v), ' x ', num2str(N_h), ' array.']);
-disp(['Linear Amplitude gain: ', num2str(a.Fa(91,181))]);
-disp(['Logarithmic Power gain: ', num2str(mag2db(a.Fa(91,181))), ' dB']);
+disp(['Linear Amplitude gain: ', num2str(lin_gain)]);
+disp(['Logarithmic Power gain: ', num2str(mag2db(lin_gain)), ' dB']);
 disp(newline)
 
 [ beamwidth_az, beamwidth_el, az_max_ang, el_max_ang ] =...
@@ -40,7 +48,7 @@ disp(['Maximum direction: ', num2str([round(az_max_ang,4), ...
 disp(newline)
 
 
-
+%% Sandra's Cell (rename :p)
 
 
 
