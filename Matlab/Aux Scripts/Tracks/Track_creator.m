91b002af13c8e2ac670de231c9f63f2ee987b1c1
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2021-07-14 17:01:52 +0100

    add warning on the time divisions (must be > 1)

--- Matlab/Aux Scripts/Tracks/Track_creator.m
+++ Matlab/Aux Scripts/Tracks/Track_creator.m
@@ -10,7 +10,16 @@
 %                 users had changed.
 
 % INPUT THE CORRECT PATH to the vars.mat of choice
-load('vars_4ues.mat'); 
+load('vars.mat'); 
+
+
+if n_time_divisions == 1
+    disp(['WARNING: there will be errors if the # time divisions is 1. ', ...
+          'This was made to glue time divisions. Create a normal track ', ...
+          'otherwise.']);
+end
+
+
 % About the vars.mat: barely any information is needed from the vars.mat
 % file. A normal simulation with that number of users with those positions
 % is enough. No need to generate the whole thing, we just need the vars.mat
